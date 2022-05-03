a356689e3297a392c8229665dec2f3bf54b3e9ad
idsme <idsmesales@gmail.com>
2022-05-03 21:50:03 +0200

    feature/updated social links

--- src/components/Social.js
+++ src/components/Social.js
@@ -3,10 +3,10 @@ const Social = () => {
         <div className="social">
             <h2>Socials</h2>
 
-            <a href="" className="button">john@email.com</a>
-            <a href="https://twitter.com/" className="button">Twitter</a>
-            <a href="https://www.linkedin.com/" className="button">LinkedIn</a>
-            <a href="https://github.com/" className="button">GitHub</a>
+            <a href="mailto:idsmesales@gmail.com" className="button">Email</a>
+            <a href="https://nl.linkedin.com/in/idsachterhof" className="button">LinkedIn</a>
+            <a href="https://www.hackerrank.com/certificates/20c4ee309a17" className="button">HackerRank</a>
+            <a href="https://github.com/idsme" className="button">GitHub</a>
         </div>
     );
 }
