feff0c0381128e47761dcea49ef2601fd08adec4
idsme <idsmesales@gmail.com>
2022-05-03 21:48:44 +0200

    feature/updating updated about

--- src/components/About.js
+++ src/components/About.js
@@ -1,19 +1,18 @@
 // Import Assets
-import profile from '../assets/profile.png';
+import profile from '../assets/profile.jpg';
 
 const About = () => {
     return (
         <section className='about'>
             <h2>About Me</h2>
             <p>
-                Lorem ipsum dolor sit amet consectetur,
-                adipisicing elit. Neque, pariatur odio.
-                Maxime, labore nihil illo ea voluptate suscipit aliquid animi
-                possimus laudantium fugit obcaecati harum?
-                Assumenda maiores nam magnam animi? Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Eaque voluptatibus vitae, rem corrupti id consectetur!
-                Sapiente laboriosam nam excepturi exercitationem natus unde, harum
-                consequatur vero amet consectetur quod sunt autem!
+                <ul>
+                    <li>Naturally curious.</li>
+                    <li>Bigger Puzzles, more fun.</li>
+                    <li>Super dad is my J-O-B</li>
+                    <li>User impact is nice</li>
+                    <li>Happy users is what I aim for</li>
+                </ul>
             </p>
         </section>
     );
