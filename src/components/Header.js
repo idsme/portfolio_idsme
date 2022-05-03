eee4d91170a2665ec570a3ff7105f5fcfe0fa669
idsme <idsmesales@gmail.com>
2022-05-03 21:47:58 +0200

    feature/updating updated personal info.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,16 @@
 // Import Assets
-import profile from '../assets/profile.png';
+import profile from '../assets/profile.jpg';
 
 const Header = () => {
     return (
         <section className='header'>
-            <img src={profile} alt="John Doe" />
+            <img src={profile} alt="Ids Achterhof" />
 
             <div className='header__content'>
-                <h1>Hi, I'm John Doe</h1>
+                <h1>Hi, I'm Ids Achterhof</h1>
                 <p>Blockchain Developer</p>
                 <button className='button'>Get In Touch</button>
+                {/*<a href="mailto:idsmesales@gmail.com" className='button'>Get In Touch</a>*/}
             </div>
         </section>
     );
