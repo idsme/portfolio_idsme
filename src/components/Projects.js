4776a258e644617f9127cb1b3c0b379d9edaa09f
idsme <idsmesales@gmail.com>
2022-05-03 21:48:26 +0200

    feature/updating updated project experience

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,6 @@
 // Import Assets
-import uniswap from '../assets/uniswap.png';
-import compound from '../assets/compound.png';
-import aave from '../assets/aave.png';
+import web3 from '../assets/WEB3_Final.jpg';
+import adhd from '../assets/ADHD_Word_Cloud_in_Blue.png';
 
 const Projects = () => {
     return (
@@ -11,39 +10,44 @@ const Projects = () => {
             <div className="projects__cards">
 
                 <div className="projects__card">
-                    <h3>Uniswap</h3>
-                    <img src={uniswap} alt="Uniswap Swap Page" />
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                        Facilis consectetur facere neque assumenda quasi aliquid,
-                        perferendis ratione rerum dolorem.
+                    <h3>Booking Web3-Dapp</h3>
+                    <img
+                        src="https://c.pxhere.com/photos/03/1d/entrepreneur_startup_start_up_man_planing_business_office_businessman-764648.jpg!d"
+                        srcSet="https://c.pxhere.com/photos/03/1d/entrepreneur_startup_start_up_man_planing_business_office_businessman-764648.jpg!d"
+                        alt="Currently working on Booking Dapp"/>
+                    <p>
+                        Why hire with fiat. (WIP)
                     </p>
 
-                    <a href="https://uniswap.org/" target="_blank" className="button">Site</a>
-                    <a href="https://github.com/Uniswap/v2-core" target="_blank" className="button">Code</a>
+                    <a href="" target="_blank" className="button">Site</a>
+                    <a href="https://github.com/idsme/portfolio_idsme" target="_blank" className="button">Code</a>
                 </div>
-
                 <div className="projects__card">
-                    <h3>Compound</h3>
-                    <img src={compound} alt="Compound Landing Page" />
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                        Facilis consectetur facere neque assumenda quasi aliquid,
-                        perferendis ratione rerum dolorem.
+                    <h3>Task Tracker Kids</h3>
+                    <img
+                        src={adhd}
+                        alt="Easy Task tracker"/>
+                    <p>
+                       The power of organized ADHD. (WIP)
                     </p>
 
-                    <a href="https://compound.finance/" target="_blank" className="button">Site</a>
-                    <a href="https://github.com/compound-finance/compound-protocol" target="_blank" className="button">Code</a>
+                    <a href="" target="_blank" className="button">Site</a>
+                    <a href="https://github.com/idsme/portfolio_idsme" target="_blank" className="button">Code</a>
                 </div>
-
                 <div className="projects__card">
-                    <h3>Aave</h3>
-                    <img src={aave} alt="Aave Landing Page" />
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                        Facilis consectetur facere neque assumenda quasi aliquid,
-                        perferendis ratione rerum dolorem.
+                    <h3>Web3 IPFS portfolio</h3>
+                    <img src={web3} alt="Uniswap Swap Page" />
+                    <p>
+                        Web3?</p>
+                    <p>
+                        Yes, this site is Web3 ;)
+                    </p>
+                    <p>
+                        What would you like to know?
                     </p>
 
-                    <a href="https://aave.com/" target="_blank" className="button">Site</a>
-                    <a href="https://github.com/aave/aave-v3-core" target="_blank" className="button">Code</a>
+                    <a href="" target="_blank" className="button">Site</a>
+                    <a href="https://github.com/idsme/portfolio_idsme" target="_blank" className="button">Code</a>
                 </div>
             </div>
         </section>
