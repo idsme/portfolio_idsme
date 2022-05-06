9d7a2c594627ff50cf889e1bbaaec9135e69336a
idsme <idsmesales@gmail.com>
2022-05-06 17:51:19 +0200

    feature/implement first versions of new social displaying icons.

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,14 +1,14 @@
 const Social = () => {
     return (
-        <div className="social">
-            <h2>Socials</h2>
-
-            <a href="mailto:idsmesales@gmail.com" className="button">Email</a>
-            <a href="https://nl.linkedin.com/in/idsachterhof" className="button">LinkedIn</a>
-            <a href="https://www.hackerrank.com/certificates/20c4ee309a17" className="button">HackerRank</a>
-            <a href="https://github.com/idsme" className="button">GitHub</a>
-        </div>
+        <>
+            <div className="social" style={{fontSize:"48px"}}>
+                {/*<h2>More info:</h2>*/}
+                <a href="mailto:idsmesales@gmail.com" style={{color: "#EA4335"}} className="icon"><i className="fa-solid fa-at"></i></a>
+                <a href="https://nl.linkedin.com/in/idsachterhof" style={{color: "#0a66c2"}} className="icon"><i className="fa fa-linkedin" aria-hidden="true"></i></a>
+                <a href="https://www.hackerrank.com/certificates/20c4ee309a17" style={{color: "#1ba94c"}} className="icon"><i className="fab fa-hackerrank"></i></a>
+                <a href="https://github.com/idsme" className="icon" style={{color: "#24292f"}} ><i className="fa-brands fa-github"></i></a>
+            </div>
+        </>
     );
 }
-
 export default Social;
